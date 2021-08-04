50dd1fa9d5d903b2053e986eb506ae47f5de01cf
Raul Barba <luar_barba@hotmail.com>
2021-08-04 01:00:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Captura de prospectos 
+
+## Información general
+
+Proyecto realizado con Angular y MySql 
+ 
+### Instrucciones de instalación
+
+1. [Requisitos](Contar con node y npm https://nodejs.org/es/download/)
+2. [Instalación](En la carpeta del proyecto instalar las librerias con el comando en consola de npm install)
+3. [Base de datos](Revisar archivo readme correspondiente)
+4. [Ejecución](En la consola dentro de la carpeta del proyecto escribir ng serve o npm start)
+
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faList } from '@fortawesome/free-solid-svg-icons';
+import { faListAlt } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'concredito-app';
+  faCoffee = faCoffee;
+  faPen = faPen;
+  faList=faList;
+  faListAlt=faListAlt;
+}
